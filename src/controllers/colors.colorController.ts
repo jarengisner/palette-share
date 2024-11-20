d71602a69f8996377a156de35ae18e95a18f0206
jarengisner <124743127+jarengisner@users.noreply.github.com>
2024-11-19 21:06:24 -0800

    began work on colors controller and also test prepping

--- src/controllers/colors.colorController.ts
+++ src/controllers/colors.colorController.ts
@@ -1,3 +1,29 @@
-/**
- * This controller will purely handle requests that involve using our colorpicker
- */
+import {
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Express } from 'express';
+import { ColorService } from 'src/services/colors.colorService';
+
+@Controller('colors')
+export class ColorController {
+  constructor(private readonly colorService: ColorService) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('photo'))
+  async processPhoto(@UploadedFile() photo: Express.Multer.File): Promise<any> {
+    try {
+      return await this.colorService.extractColorFileUpload(photo);
+    } catch (err) {
+      throw new HttpException(
+        `Error in controller for processing a photo: ${err.message}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
--- src/modules/users.module.ts
+++ src/modules/users.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserController } from 'src/controllers/users.userController';
 import { UserModel } from 'src/models/user.userModel';
 import { UserService } from 'src/services/users.userService';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserModel }])],
-  controllers: [],
+  controllers: [UserController],
   providers: [UserService],
 })
 export class UserModule {}
